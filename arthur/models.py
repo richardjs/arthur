45d67c858117e410e9fc4cfae0b8b630d22d921a
Richard Schneider <richard@schneiderbox.net>
2023-01-08 17:22:25 -0600

    Expand data model

--- arthur/models.py
+++ arthur/models.py
@@ -1,8 +1,35 @@
 from django.db import models
 
 
+class Game(models.Model):
+    players = models.ManyToManyField('Player', through='GamePlayer')
+
+    start_timestamp = models.DateTimeField(auto_now_add=True)
+    end_timestamp = models.DateTimeField(null=True)
+
+    class Status(models.TextChoices):
+        IN_PROGRESS = 'I'
+        FINISHED = 'F'
+        ERROR = 'E'
+        ABANDONED = 'A'
+    status = models.CharField(
+        max_length=1,
+        choices=Status.choices,
+        default=Status.IN_PROGRESS,
+    )
+
+
+class GamePlayer(models.Model):
+    game = models.ForeignKey('Game', on_delete=models.CASCADE)
+    player = models.ForeignKey('Player', on_delete=models.CASCADE)
+
+    number = models.IntegerField()
+    winner = models.BooleanField(default=False)
+
+
 class Player(models.Model):
     name = models.TextField(null=True, blank=True)
+    repository = models.TextField()
     commit = models.CharField(max_length=40)
     invocation = models.TextField()
 
